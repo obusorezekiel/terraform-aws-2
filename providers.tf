1be211e2c26c94a2d8efd84a62f4d859638309c5
Ezekiel Umesi <obusorezekiel@gmail.com>
2023-02-18 00:30:48 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = "~> 0.12.18"
+  backend "s3" {
+    bucket = "terraform-store-e"
+    key    = "terraform-store/terraform"
+    region = "eu-west-2"
+  }
 }
 
 provider "aws" {
